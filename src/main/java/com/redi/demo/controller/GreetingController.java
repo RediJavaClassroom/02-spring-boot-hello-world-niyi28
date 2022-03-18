7947d84b0d181fa5747577acb01fb6fd381affd6
niyi28 <61952240+niyi28@users.noreply.github.com>
2022-03-18 21:43:23 +0100

    Initial commit

--- src/main/java/com/redi/demo/controller/GreetingController.java
+++ src/main/java/com/redi/demo/controller/GreetingController.java
@@ -1,13 +1,25 @@
 package com.redi.demo.controller;
 
+import com.redi.demo.domain.StatisticProcessor;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class GreetingController {
-
-    @GetMapping("greeting")
-    public String greet() {
-        return "Hello, World!";
+    Map <String, Long> greetingStats = new HashMap<>();
+    JSONArray jsonArray = new JSONArray();
+    @GetMapping("statistics")
+    public String greet(@RequestParam(value = "statsAddress") String name) throws JSONException {
+        Long count =  StatisticProcessor.processStats(name, greetingStats);
+        greetingStats.put(name, count);
+        jsonArray = StatisticProcessor.publishGreetingStats(greetingStats);
+        return jsonArray.toString();
     }
 }
+
---
+++ src/main/java/com/redi/demo/domain/StatisticProcessor.java
@@ -0,0 +1,30 @@
+package com.redi.demo.domain;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class StatisticProcessor {
+    public static Long processStats(String name, Map<String, Long> greetingStats){
+        long count = new AtomicLong().incrementAndGet();
+        if (greetingStats.containsKey(name)) {
+            count = count + greetingStats.get(name);
+        }
+        return count;
+    }
+
+    public static JSONArray publishGreetingStats(Map <String, Long> greetingStats) throws JSONException {
+        JSONArray jsonArray = new JSONArray();
+        for (Map.Entry<String, Long> entry : greetingStats.entrySet()){
+            JSONObject forEveryEntry = new JSONObject();
+            forEveryEntry.put("name", entry.getKey());
+            forEveryEntry.put("greeting", entry.getValue());
+            jsonArray.put(forEveryEntry);
+        }
+        return jsonArray;
+    }
+}
+
